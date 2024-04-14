3993ac7beeafb57ca4e57ce453a479f5ed4f083f
suraj sangale <surajdsangale@gmail.com>
2024-04-14 20:46:05 +0530

    format

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import "./About.css"
-import { aboutMeText, images } from './Data';
+import React from "react";
+import "./About.css";
+import { aboutMeText, images } from "./Data";
 const About = () => {
   return (
-    <div className='about container section' id='about'><h2 className='section__title text-white'>
-      About Me
-    </h2>
+    <div className="about container section" id="about">
+      <h2 className="section__title text-white">About Me</h2>
       <div className=" mx-auto my-8 p-8 bg-white rounded-lg shadow-md w-10/12		">
         <div className="mb-6">
-          <p className='about__description'>
-            {aboutMeText}</p>
-          <h2 className='section__subtitle text-slate-800 text-center'>Skills</h2>
+          <p className="about__description">{aboutMeText}</p>
+          <h2 className="section__subtitle text-slate-800 text-center">
+            Skills
+          </h2>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {images.map((item, index) => (
-
             <div
               key={index}
               className="relative overflow-hidden  flex flex-col justify-center items-center h-20 m-1 rounded-md transition-transform transform hover:scale-105"
@@ -23,17 +22,22 @@ const About = () => {
                 src={item.imgUrl}
                 alt={`Image ${item.id}`}
                 className="icons group max-h-full max-w-full object-cover transition-transform transform duration-300 group-hover:scale-120"
-                style={{ maxHeight: '83%', maxWidth: '100%' }}
+                style={{ maxHeight: "83%", maxWidth: "100%" }}
               />
               <p className="mb-1 text-center text-sm">{item.name}</p>
             </div>
-
           ))}
         </div>
-        <a href='https://drive.google.com/file/d/1E0HIyQVYQ_GtvAkp41KPo33kc9Oa_Pjw/view?usp=drive_link' target="_blank" className='btn mt-4 text-center'>Download Resume</a>
+        <a
+          href="https://drive.google.com/file/d/1E0HIyQVYQ_GtvAkp41KPo33kc9Oa_Pjw/view?usp=drive_link"
+          target="_blank"
+          className="btn mt-4 text-center"
+        >
+          Download Resume
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
