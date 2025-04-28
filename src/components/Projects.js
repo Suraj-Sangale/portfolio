80d59c42fca314982fe85b1a0c48d1924267a60b
Suraj Sangale <89401382+Suraj-Sangale@users.noreply.github.com>
2025-04-28 11:27:42 +0530

    Update Projects array for Point Stream Panel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,72 +10,72 @@ export default function Projects() {
     setHoveredIndex(state ? index : null);
   };
 
-  const MY_PROJECTS = [
-    {
-      title: "Urban Market",
-      description:
-        "I built a fully responsive clone of Urban Marketplace, a modern e-commerce platform where users can explore a wide variety of products, add items to their cart, and seamlessly place orders.",
-      keyFeatures: [
-        { id: 1, text: "Modern and responsive UI for smooth product browsing" },
-        { id: 2, text: "Add to cart and complete checkout functionality" },
-        {
-          id: 3,
-          text: "Built with React.js and Tailwind CSS, deployed on Netlify",
-        },
-        { id: 1, text: "Modern and responsive UI for smooth product browsing" },
-        { id: 2, text: "Add to cart and complete checkout functionality" },
-        {
-          id: 3,
-          text: "Built with React.js and Tailwind CSS, deployed on Netlify",
-        },
-      ],
+ const MY_PROJECTS = [
+	{
+		title: "Urban Market",
+		description:
+			"I built a fully responsive clone of Urban Marketplace, a modern e-commerce platform where users can explore a wide variety of products, add items to their cart, and seamlessly place orders.",
+		keyFeatures: [
+			{ id: 1, text: "Modern and responsive UI for smooth product browsing" },
+			{ id: 2, text: "Add to cart and complete checkout functionality" },
+			{
+				id: 3,
+				text: "Built with React.js and Tailwind CSS, deployed on Netlify",
+			},
+			{ id: 1, text: "Modern and responsive UI for smooth product browsing" },
+			{ id: 2, text: "Add to cart and complete checkout functionality" },
+			{
+				id: 3,
+				text: "Built with React.js and Tailwind CSS, deployed on Netlify",
+			},
+		],
 
-      techStack: ["Next.js", "Tailwind CSS", "Node.js"],
-      image: "UrbanMarket.png",
-      gitUrl: "https://github.com/Suraj-Sangale/urban-market",
-      liveUrl: "https://urbanmarketplace.netlify.app/home",
-    },
-    {
-      title: "Point Stream Panel",
-      description:
-        "Developed a web-based platform to efficiently manage sports tournaments and matches.Key features include match setup booths, real-time scoreboard updates, multi-referee score management, and match history tracking.",
-      keyFeatures: [
-        {
-          id: 1,
-          text: "Optimized performance with server-side rendering (SSR) and static site generation (SSG).",
-        },
-        {
-          id: 2,
-          text: "Built backend API with JWT auth, WebSocket support, and MySQL integration.",
-        },
-        {
-          id: 3,
-          text: "Enabled real-time score updates from multiple referees for accurate match tracking.",
-        },
-      ],
+		techStack: ["Next.js", "Tailwind CSS", "Node.js"],
+		image: "UrbanMarket.png",
+		gitUrl: "https://github.com/Suraj-Sangale/urban-market",
+		liveUrl: "https://urbanmarketplace.netlify.app/home",
+	},
+	{
+		title: "Point Stream Panel",
+		description:
+			"A dynamic web-based platform built to streamline the management of sports tournaments, matches, and real-time scoring. Key highlights include match booth setups, referee-based access control, live scoreboard updates from multiple devices, detailed match history with obtained scores, and automated scorecard generation.",
+		keyFeatures: [
+			{
+				id: 1,
+				text: "Blazing-fast performance with Server-Side Rendering (SSR) and Static Site Generation (SSG) using Next.js.",
+			},
+			{
+				id: 2,
+				text: "Robust backend API with JWT authentication, WebSocket-powered real-time communication, and seamless MySQL database integration.",
+			},
+			{
+				id: 3,
+				text: "Real-time multi-referee scoring system with secured role-based access, ensuring accurate and transparent match updates.",
+			},
+		],
+		techStack: ["Next.js", "Tailwind CSS", "Socket.io", "MySQL", "Node.js"],
+		image: "PSPboard.png",
+		liveUrl: "https://board.agni-byte.com",
+	},
+	{
+		title: "Memo Mind",
+		description:
+			"A web-based platform designed to enhance memory retention and learning through interactive flashcards and quizzes. Users can create, share, and study flashcards on various topics, making learning engaging and effective.",
+		keyFeatures: [
+			{ id: 1, text: "Create and share flashcards for effective learning" },
+			{ id: 2, text: "Interactive quizzes to reinforce knowledge retention" },
+			{
+				id: 3,
+				text: "User-friendly interface for easy navigation and study",
+			},
+		],
 
-      techStack: ["Next.js", "Tailwind CSS", "Node.js"],
-      image: "PSPboard.png",
-      liveUrl: "https://board.agni-byte.com",
-    },
-    {
-      title: "Memo Mind",
-      description:
-        "A web-based platform designed to enhance memory retention and learning through interactive flashcards and quizzes. Users can create, share, and study flashcards on various topics, making learning engaging and effective.",
-      keyFeatures: [
-        { id: 1, text: "Create and share flashcards for effective learning" },
-        { id: 2, text: "Interactive quizzes to reinforce knowledge retention" },
-        {
-          id: 3,
-          text: "User-friendly interface for easy navigation and study",
-        },
-      ],
+		techStack: ["Next.js", "Tailwind CSS", "Node.js"],
+		image: "memoMind.png",
+		liveUrl: "https://memo-mind-self.vercel.app",
+	},
+];
 
-      techStack: ["Next.js", "Tailwind CSS", "Node.js"],
-      image: "memoMind.png",
-      liveUrl: "https://memo-mind-self.vercel.app",
-    },
-  ];
   return (
     <div
       className="proj section relative"
