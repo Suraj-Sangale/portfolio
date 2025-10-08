b6c8f2116a9ca30b27f939a5897b9cfe2acb84e6
Suraj <surajsangle00@gmail.com>
2025-10-08 18:20:40 +0530

    swiper added

---
+++ utilities/customSwiper.js
@@ -0,0 +1,58 @@
+import { useId } from "react";
+import { Swiper } from "swiper/react";
+import { Navigation, Pagination, Autoplay, EffectCoverflow, EffectCards } from "swiper/modules";
+
+import commonStyle from "@/styles/common.module.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+
+export default function CustomSwiper({ children, carouselOptions, className = "" }) {
+	const id = useId();
+
+	const options = {
+		slidesPerView: 1,
+		centeredSlides: false,
+		spaceBetween: 0,
+		loop: false,
+		rewind: false,
+		pagination: false,
+		navigation: carouselOptions?.navigation
+			? {
+					nextEl: `.swiper-instance-${id} .swiper-button-next`,
+					prevEl: `.swiper-instance-${id} .swiper-button-prev`,
+			  }
+			: false,
+		onSlideChange: () => {},
+		onSwiper: (swiper) => {
+			// Force autoplay start on mobile after a short delay
+			if (carouselOptions?.autoplay && swiper.autoplay) {
+				setTimeout(() => {
+					swiper.autoplay?.start();
+				}, 100);
+			}
+		},
+		onInit: (swiper) => {
+			// Ensure autoplay starts on initialization
+			if (carouselOptions?.autoplay && swiper.autoplay) {
+				swiper.autoplay?.start();
+			}
+		},
+		modules: [Navigation, Pagination, Autoplay, EffectCoverflow, EffectCards],
+		...carouselOptions,
+	};
+
+	return (
+		<>
+			{carouselOptions === false ? (
+				<>{children}</>
+			) : (
+				<Swiper
+					className={commonStyle["swiper"] + ` swiper-instance-${id} ${className}`}
+					{...options}
+				>
+					{children}
+				</Swiper>
+			)}
+		</>
+	);
+}
