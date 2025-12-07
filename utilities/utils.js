4429f52c210d7e37d5d24d101c4a012d5c4450f3
Suraj <surajsangle00@gmail.com>
2025-12-07 23:11:49 +0530

    common function add

--- utilities/utils.js
+++ utilities/utils.js
@@ -3,3 +3,11 @@ import { constantsList } from "./constant";
 export const getConstant = (key) => {
   return constantsList[key.toUpperCase()] ?? null;
 };
+export const toSnakeCase = (text) => {
+  if (!text) return "";
+  return text
+    .replace(/([a-z])([A-Z])/g, "$1_$2") // handle camelCase → snake_case
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "_");
+};
