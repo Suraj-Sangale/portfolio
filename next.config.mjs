2b81420f2d1e9c7fe5a4f00e4a205b348140d856
Suraj <surajsangle00@gmail.com>
2026-02-13 09:20:15 +0530

    use home page data from redis

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
+  // Exclude server-only packages from client bundle
+  serverExternalPackages: ['ioredis'],
   images: {
     remotePatterns: [
       {
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
+import { getPortfolioDataController } from "@/backend/controller/commonController";
 import Header from "@/components/header";
 import HomeWrapper from "@/components/homeWrapper";
 
-export default function Home() {
+export default function Home({ pageData }) {
   return (
     <>
       <Header />
-      <HomeWrapper />
+      <HomeWrapper pageData={pageData} />
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  // const pageData = await fetch("http://localhost:3000/api/portfolio").then((res) => res.json());
+  const cacheKey = "portfolio:all";
+  const pageData = {};
+
+  const [homePageData] = await Promise.all([
+    getPortfolioDataController(cacheKey),
+  ]);
+
+  // console.log("homePageData", homePageData);
+  if (homePageData.status) {
+    pageData.homePageData = homePageData.data;
+  }
+
+  return {
+    props: {
+      pageData,
+    },
+  };
+};
