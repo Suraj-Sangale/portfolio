26b8374e64493492c06bd001d883a67103e9e4c8
Suraj <surajsangle00@gmail.com>
2026-02-20 04:42:47 +0530

    api path changed

--- pages/api/blogs/medium.js
+++ pages/api/blogs/medium.js
@@ -23,6 +23,11 @@ import Parser from "rss-parser";
 // }
 
 export default async function handler(req, res) {
+  // Support both GET and POST methods
+  if (req.method !== "GET" && req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
   try {
     const response = await fetch(
       "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@surajsangle00"
