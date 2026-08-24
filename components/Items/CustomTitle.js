a26b30b77b567284573b09ca9940070056c80a03
Suraj <surajsangle00@gmail.com>
2026-08-24 16:12:30 +0530

    title

--- components/Items/CustomTitle.js
+++ components/Items/CustomTitle.js
@@ -1,18 +1,20 @@
 import React from "react";
+import styles from "@/styles/header.module.scss";
 
 const CustomTitle = ({
   subheading = "",
   mainText = "",
   highlightedText = "",
+  className = ""
 }) => {
   // checl
   return (
-    <div className="text-container ml-[4%] md:ml-0">
-      {subheading && <p className="subheading">{subheading}</p>}
-      <h1 className="main-heading">
-        {mainText && <span className="white-text">{mainText} </span>}
+    <div className={styles.textContainer + "  ml-[4%] md:ml-0 " + className}>
+      {subheading && <p className={styles.subheading}>{subheading}</p>}
+      <h1 className={styles.mainHeading}>
+        {mainText && <span className={styles.whiteText}>{mainText} </span>}
         {highlightedText && (
-          <span className="gradient-text">{highlightedText}</span>
+          <span className={styles.gradientText}>{highlightedText}</span>
         )}
       </h1>
     </div>
